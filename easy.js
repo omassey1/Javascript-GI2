9d8d470ce38d858e44fbfd8b1f65ecdf32f897ef
omassey1 <65238159+omassey1@users.noreply.github.com>
2022-09-19 13:01:30 -0400

    Tay

---
+++ easy.js
@@ -0,0 +1,16 @@
+const student1 = [];
+student1[0]= "Octevion";
+student1[1]= "Massey";
+student1[2]= 22;
+
+const student2 = [];
+student2[0]= "Jimmy";
+student2[1]= "Neutron";
+student2[2]= 11;
+
+const student3 = [];
+student3[0]= "Timmy";
+student3[1]= "Turner";
+student3[2]= 10;
+
+console.log("Hello my name is", student2[0], student2[1], "and I am", student2[2], "years old!")
